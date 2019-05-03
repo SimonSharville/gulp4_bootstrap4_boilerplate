3a81add5dfca85cc5a7725642c92b75f5c811028
Simon Sharville <git@simonsharville.co.uk>
2019-05-03 13:49:53 +0200

    Revert Roboto font to be requested from remote

--- gulpfile.js
+++ gulpfile.js
@@ -127,10 +127,11 @@ function imageDelete(){
 }
 
 // Fontawesome
-function faIcons() {
-    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
-        .pipe(gulp.dest('docs/assets/webfonts/'));
-};
+// Only needed if fonts should be local
+// function faIcons() {
+//     return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+//         .pipe(gulp.dest('docs/assets/webfonts/'));
+// };
 
 
 // /////////////////////////////
@@ -152,7 +153,7 @@ function watch(done) {
   gulp.watch('app/assets/js/sardJS.js', minifyJS).on('change', browserSync.reload);
   gulp.watch(imgSRC, imageDelete).on('change', browserSync.reload);
   gulp.watch(imgSRC, minifyImages).on('change', browserSync.reload);
-  gulp.watch('node_modules/@fortawesome/fontawesome-free/webfonts/*', faIcons).on('change', browserSync.reload);
+  // gulp.watch('node_modules/@fortawesome/fontawesome-free/webfonts/*', faIcons).on('change', browserSync.reload);
 }
 
 
@@ -162,7 +163,7 @@ exports.concatCSS = concatCSS;
 exports.js = js;
 exports.minifyImages = minifyImages;
 exports.minifyJS = minifyJS;
-exports.faIcons = faIcons;
+// exports.faIcons = faIcons;
 exports.watch = watch;
 
 // Sets the default to gulp watch
