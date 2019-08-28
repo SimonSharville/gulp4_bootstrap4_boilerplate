11f73ee895481c81b3b771e98221467b11770527
Simon Sharville <git@simonsharville.co.uk>
2019-08-28 12:41:52 +0100

    Modal Popovers

--- docs/assets/js/remote-sard-scripts.js
+++ docs/assets/js/remote-sard-scripts.js
@@ -17674,6 +17674,11 @@ $(function() {
     $('[data-toggle="tooltip"]').tooltip(); 
 });
 
+}).done(function(data) {
+  $(".modal-content").html($(data).find('.inner_modal'));
+  $(".modal-header").prepend('<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>');
+  $('[data-toggle="popover"]').popover({trigger: "hover",  placement: 'auto left'});
+});
 
 // Add Local Scripts here
 
