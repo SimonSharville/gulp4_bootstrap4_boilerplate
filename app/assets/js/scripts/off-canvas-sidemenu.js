8c4ac26028ba1b2d6312870b67dd57e7dcd9c5f9
Simon Sharville <git@simonsharville.co.uk>
2019-11-12 12:19:37 +0000

    Side Menu - Build and style

--- app/assets/js/scripts/off-canvas-sidemenu.js
+++ app/assets/js/scripts/off-canvas-sidemenu.js
@@ -5,14 +5,26 @@
 $(document).ready(function(){                       
    
   // sa-sidemenu Menu Slider
-   $('#sa-sidemenu-nav-expander').on('click',function(e){
-     e.preventDefault();
-     $('body').toggleClass('sa-sidemenu-nav-expanded');
-   });
-   $('#sa-sidemenu-nav-close').on('click',function(e){
-     e.preventDefault();
-     $('body').removeClass('sa-sidemenu-nav-expanded');
-   });
+  $('.sa-sidemenu').on('click',function(e){
+    e.preventDefault();
+    $('.sa-sidemenu').toggleClass('open');
+  });
+
+  // $('.sa-sidemenu').hover(function(e){
+  //   e.preventDefault();
+  //   $('.sa-sidemenu').toggleClass('open');
+  // });
+
+  //  $('#sa-sidemenu-nav-expander').on('click',function(e){
+  //    e.preventDefault();
+  //    $('body').toggleClass('sa-sidemenu-nav-expanded');
+  //  });
+
+   
+  //  $('#sa-sidemenu-nav-close').on('click',function(e){
+  //    e.preventDefault();
+  //    $('body').removeClass('sa-sidemenu-nav-expanded');
+  //  });
    
    // Only needed if we use dropdowns within the menu
 
